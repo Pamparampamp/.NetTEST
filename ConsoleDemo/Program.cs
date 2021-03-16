cc1a6b17e0cffe84d4c519b1781c548c3fe9c9e6
arnas112 <72792606+arnas112@users.noreply.github.com>
2021-03-16 17:52:36 +0200

    first commit

---
+++ ConsoleDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+          
+            if (true == true)
+            {
+                Console.WriteLine($"Input was: { 2 }");
+
+            }
+            else
+            {
+                Console.WriteLine("Error while parssing");
+            }
+            var numbers = new List<int>()
+            {
+                1,2,3
+            };
+            foreach(int number in numbers)
+            {
+                Console.WriteLine($"number is : { number}");
+            }
+
+            string operatorInput = "-";
+            if(operatorInput == "2")
+            {
+
+            }
+
+            while (true)
+            {
+                Console.WriteLine($"number is : { 4}");
+            }
+
+        }
+
+    }
+}
